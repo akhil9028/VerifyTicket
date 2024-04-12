05b02c1fc4a52edf5989514e1e0e4ab5900f3bf1
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2024-04-12 13:09:49 +0530

    Update verified.js

--- verified.js
+++ verified.js
@@ -1,14 +1,24 @@
+console.log("value = " + totalFare);
 var urlParams = new URLSearchParams(window.location.search);
 var totalFare = urlParams.get("totalFare");
 var tickets = urlParams.get("tickets");
 var idd = urlParams.get("id");
-console.log("value = " + totalFare);
+var time = urlParams.get("time");
+
+var currentDate = new Date();
+
+var dateString = currentDate.toLocaleDateString("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+var timeString = currentDate.toLocaleTimeString("en-US");
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("passPrice").textContent = totalFare;
+  document.getElementById("totalFare").textContent = totalFare;
   document.getElementById("bus-id").textContent = idd;
-  // document.getElementById("time").textContent = timeString;
-  // document.getElementById("date").textContent = dateString;
+  document.getElementById("time").textContent = time;
+  document.getElementById("date").textContent = dateString;
   document.getElementById("tickets").textContent = tickets;
 
   console.log("totalFare = " + totalFare);
