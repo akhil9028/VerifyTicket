b2d0bad820ce927b67cb7dc1e9e190ad2e0c1a5b
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2024-04-12 15:36:03 +0530

    Update verified.js

--- verified.js
+++ verified.js
@@ -1,8 +1,9 @@
 console.log("value = " + totalFare);
 var urlParams = new URLSearchParams(window.location.search);
+var sum = urlParams.get("sum");
 var totalFare = urlParams.get("totalFare");
 var tickets = urlParams.get("tickets");
-var idd = urlParams.get("id");
+var idd;
 var time = urlParams.get("time");
 
 var currentDate = new Date();
@@ -14,8 +15,36 @@ var dateString = currentDate.toLocaleDateString("en-US", {
 });
 var timeString = currentDate.toLocaleTimeString("en-US");
 
+var sum = totalFare;
+
+var num = Number(sum);
+
+var num = parseInt(sum);
+
+idd = num % 1000;
+num = num / 1000;
+tickets = Math.floor(num % 100);
+
+if (tickets >= 10 && tickets < 20) {
+  fare = num / 100;
+} else if (tickets > 20) {
+  tickets = tickets % 10;
+  fare = num / 100;
+} else {
+  fare = num / 10;
+}
+fare = Math.floor(fare);
+
+console.log(idd);
+console.log(tickets);
+console.log(fare);
+
+console.log(idd);
+console.log(totalFare);
+console.log(tickets);
+
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("totalFare").textContent = totalFare;
+  document.getElementById("totalFare").textContent = fare;
   document.getElementById("bus-id").textContent = idd;
   document.getElementById("time").textContent = time;
   document.getElementById("date").textContent = dateString;
